558bc11b84136cd57aae68ba037c5cf8d0ed7168
Jason Devaraj <jdevaraj01@gmail.com>
2025-03-18 15:47:37 -0400

    Added pthread.

--- app.cpp
+++ app.cpp
@@ -1,7 +1,12 @@
-// Write a Program using Mutex and Pthread, where Thread_1 writes information into a Shared Variable and Thread_2 reads Information from that Shared Data. Submit your GitHub link for your program.
+// Write a Program using Mutex and Pthread, where Thread_1 writes 
+//information into a Shared Variable and Thread_2 reads Information 
+//from that Shared Data. Submit your GitHub link for your program.
 #include <iostream>
+#include <thread>
+#include <mutex>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    pthread_t Thread_1;
+    pthread_t Thread_2;
     return 0;
 }
