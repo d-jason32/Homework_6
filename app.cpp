d208a9538840641a762a093b367c5ea133778dd0
Jason Devaraj <jdevaraj01@gmail.com>
2025-03-17 21:34:56 -0400

    first commit

---
+++ app.cpp
@@ -0,0 +1,7 @@
+// Write a Program using Mutex and Pthread, where Thread_1 writes information into a Shared Variable and Thread_2 reads Information from that Shared Data. Submit your GitHub link for your program.
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
